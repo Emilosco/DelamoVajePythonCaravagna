392e056c0906e61303f1cf7dba86c08f103c7689
emilosco <95098658+Emilosco@users.noreply.github.com>
2022-02-04 11:34:15 +0000

    Emil: Sem nrdu Classe Autobus

--- emil16_1234.py
+++ emil16_1234.py
@@ -3,9 +3,24 @@ class Veicolo:
         self.modello= modello
         self.velocita= velocita
         self.km= km
+        
 
     def CaratteristicheVeicolo(self):
         print("Caratteristiche Veicolo:\n\nModello: {0}\nVelocita massima: {1}\nKm: {2}".format(self.modello, self.velocita,self.km))
 
-veicolo = Veicolo("Rolls Royce","250 km\h", "250.000 km")
+class Autobus(Veicolo):
+    def __init__(self, modello, velocita, km, capienza, rotta):
+        super().__init__(modello,velocita,km)
+        self.capienza= capienza
+        self.rotta= rotta
+    
+    def CaratteristicheAutobus(self):
+        print("\n\nCaratteristiche Veicolo:\n\nModello: {0}\nVelocita massima: {1}\nKm: {2}\nCapienza: {3}\nRotta: {4}".format(self.modello, self.velocita,self.km,self.capienza, self.rotta))
+
+#esercizio 16.1
+veicolo = Veicolo("Alfa Romeo","180 km\h", "250.000 km")
 veicolo.CaratteristicheVeicolo()
+autobus = Autobus("Alfa Romeo", "70 km\h", "300.000 km", "75 persone", "Piazzale Gioberti>> Via Giulia>> Via Battisti>> Piazza Goldoni>> Largo Irneri")
+autobus.CaratteristicheAutobus()
+#esercizio 16.2
+auto= Auto("Giulia", "180 km\h", "300.000 km", 
--- fra16_7.py
+++ fra16_7.py
@@ -1,8 +1,15 @@
+from datetime import date
+
 class Viaggio:
-    def __init__(self, nome_viaggio, data_partenza, data_ritorno, localita, resort, prezzo, partecipanti, responsabile_viaggio):
+    def __init__(self, nome_viaggio, giorno_p, mese_p, anno_p, giorno_r, mese_r, anno_r, localita, resort, prezzo, partecipanti, responsabile_viaggio):
         self.nome_viaggio = nome_viaggio
-        self.data_partenza = data_partenza
-        self.data_ritorno = data_ritorno
+        self.giorno_p = giorno_p
+        self.mese_p = mese_p
+        self.anno_p = anno_p
+        self.giorno_r = giorno_r
+        self.mese_r = mese_r
+        self.anno_r = anno_r 
+        
         self.localita = localita
         self.resort = resort
         self.prezzo = prezzo
@@ -10,7 +17,13 @@ class Viaggio:
         self.responsabile_viaggio = responsabile_viaggio
 
     def stampa(self):
-        print("Informazioni viaggio:\n\nTitolo: {0}\nPartenza: {1}\nRitorno: {2}\nLocalità: {3}\nResort: {4}\nPrezzo: {5}\nPartecipanti: {6}\nResponsabile viaggio: {7}".format(self.nome_viaggio,self.data_partenza,self.data_ritorno,self.localita,self.resort,self.prezzo,self.partecipanti,self.responsabile_viaggio))
+        print("Informazioni viaggio:\n\nTitolo: {0}\nPartenza: {1}\nRitorno: {2}\nLocalità: {3}\nResort: {4}\nPrezzo: {5}\nPartecipanti: {6}\nResponsabile viaggio: {7}".format(self.nome_viaggio,self.giorno_p,self.giorno_r,self.localita,self.resort,self.prezzo,self.partecipanti,self.responsabile_viaggio))
+
+    def periodo(self):
+        self.data_partenza = date(self.giorno_p, self.mese_p, self.anno_p)
+        self.data_ritorno = date(self.giorno_r, self.mese_r, self.anno_r)
+        delta = self.data_ritorno - self.data_partenza
+        print('Giorni di vacanza programmati: {0}'.format( (delta.days) ))
 
 
 class Vacanza_invernale(Viaggio):
@@ -31,9 +44,11 @@ class Vacanza_estiva(Viaggio):
         self.escursioni_marine = escursioni_marine
 
 print("\n===============1°=VIAGGIO=================")
-viaggio1 = Viaggio('Avventura nella giungla', '23/06/2034', '12/07/2034', 'Burundi', 'Uga Buga', '245€', 'Pino, Ugo, Gianfranca', 'Giuliano Caravagno')
+viaggio1 = Viaggio('Avventura nella giungla', [23,6,2034], [12,7,2034], 'Burundi', 'Uga Buga', '245€', 'Pino, Ugo, Gianfranca', 'Giuliano Caravagno')
 viaggio1.stampa()
+viaggio1.periodo()
 
 print("\n===============2°=VIAGGIO=================")
-viaggio_invernale = Vacanza_invernale('Rotoliamo nella neve!', '15/11/2024', '14/12/2024', 'Triglav', 'Resort Triglav', '670€', 'Bruno, Jože, Svetina', 'Stefano Alberto Sloveno', 56, 'Triglavski smučarski park')
+viaggio_invernale = Vacanza_invernale('Rotoliamo nella neve!', (15,11,2024), (14,12,2024), 'Triglav', 'Resort Triglav', '670€', 'Bruno, Jože, Svetina', 'Stefano Alberto Sloveno', 56, 'Triglavski smučarski park')
 viaggio_invernale.stampa()
+viaggio_invernale.periodo()
