4d7b777e89acb44164c2b999c5dd5bdfc0b7a04e
emilosco <95098658+Emilosco@users.noreply.github.com>
2022-02-04 12:41:49 +0000

    Emil: Sem nrdu classe Auto

--- emil16_1234.py
+++ emil16_1234.py
@@ -1,26 +1,47 @@
 class Veicolo:
-    def __init__(self, modello, velocita,km):
+    def __init__(self, modello, velocita,km, anno, revisionata):
         self.modello= modello
         self.velocita= velocita
         self.km= km
-        
+        self.anno= 2020
+        self.revisionata=2019
+
+        if self.anno < 2021:
+            print("\nLa revisione è valida, non preoccuparti!")
+        elif self.anno == 2021:
+            print("\nLa revisione scadrà quest'anno, controlla quando l'hai fatta!")
+        else:
+            print("\nLa revisione è scaduta, devi rinovarla!")
 
     def CaratteristicheVeicolo(self):
-        print("Caratteristiche Veicolo:\n\nModello: {0}\nVelocita massima: {1}\nKm: {2}".format(self.modello, self.velocita,self.km))
+        print("Caratteristiche Veicolo:\n\nModello: {0}\nVelocita massima: {1}\nKm: {2}\nAnno corrente: {3}".format(self.modello, self.velocita,self.km, self.anno))
 
 class Autobus(Veicolo):
-    def __init__(self, modello, velocita, km, capienza, rotta):
-        super().__init__(modello,velocita,km)
+    def __init__(self, modello, velocita, km, capienza, rotta,revisionata, anno):
+        super().__init__(modello,velocita,km,anno, revisionata)
         self.capienza= capienza
         self.rotta= rotta
     
     def CaratteristicheAutobus(self):
-        print("\n\nCaratteristiche Veicolo:\n\nModello: {0}\nVelocita massima: {1}\nKm: {2}\nCapienza: {3}\nRotta: {4}".format(self.modello, self.velocita,self.km,self.capienza, self.rotta))
+        print("\n\nCaratteristiche Autobus:\n\nModello: {0}\nVelocita massima: {1}\nKm: {2}\nCapienza: {3}\nRotta: {4}\nUltima Revisione: {5} \nAnno corrente: {6}".format(self.modello, self.velocita,self.km,self.capienza, self.rotta,self.revisionata, self.anno))
+    
+class Auto(Veicolo):
+    def __init__(self, modello, velocita, km, capienza, revisionata, anno):
+        super().__init__(modello,velocita,km,anno,revisionata)
+        self.capienza= capienza
+        
+
+    def CaratteristicheAuto(self):
+        print("\n\nCaratteristiche Auto:\n\nModello: {0}\nVelocita massima: {1}\nKm: {2}\nCapienza: {3}\nUltima Revisione: {4}\nAnno corrente: {5}".format(self.modello, self.velocita,self.km,self.capienza,self.revisionata,self.anno))
+
+
+
 
 #esercizio 16.1
-veicolo = Veicolo("Alfa Romeo","180 km\h", "250.000 km")
+veicolo = Veicolo("Alfa Romeo","180 km\h", "250.000 km", "2022","2019")
 veicolo.CaratteristicheVeicolo()
-autobus = Autobus("Alfa Romeo", "70 km\h", "300.000 km", "75 persone", "Piazzale Gioberti>> Via Giulia>> Via Battisti>> Piazza Goldoni>> Largo Irneri")
+autobus = Autobus("Alfa Romeo 110A", "70km\h","300.000 km","75 persone", "Piazzale Gioberti,Via Giulia, Via Battisti,Piazza Goldoni,Largo Irneri", "2019", "duemila e qualcosa")
 autobus.CaratteristicheAutobus()
 #esercizio 16.2
-auto= Auto("Giulia", "180 km\h", "300.000 km", 
+auto= Auto("Giulia", "180 km\h", "300.000 km","4 persone","2022", "duemila e qualcosa")
+auto.CaratteristicheAuto()
--- fra16_7.py
+++ fra16_7.py
@@ -3,6 +3,7 @@ from datetime import date
 class Viaggio:
     def __init__(self, nome_viaggio, giorno_p, mese_p, anno_p, giorno_r, mese_r, anno_r, localita, resort, prezzo, partecipanti, responsabile_viaggio):
         self.nome_viaggio = nome_viaggio
+        
         self.giorno_p = giorno_p
         self.mese_p = mese_p
         self.anno_p = anno_p
@@ -13,42 +14,108 @@ class Viaggio:
         self.localita = localita
         self.resort = resort
         self.prezzo = prezzo
+        
         self.partecipanti = partecipanti
         self.responsabile_viaggio = responsabile_viaggio
 
     def stampa(self):
-        print("Informazioni viaggio:\n\nTitolo: {0}\nPartenza: {1}\nRitorno: {2}\nLocalità: {3}\nResort: {4}\nPrezzo: {5}\nPartecipanti: {6}\nResponsabile viaggio: {7}".format(self.nome_viaggio,self.giorno_p,self.giorno_r,self.localita,self.resort,self.prezzo,self.partecipanti,self.responsabile_viaggio))
+        print("Informazioni viaggio:\n\nTitolo: {0}\nPartenza: {1}/{2}/{3}\nRitorno: {4}/{5}/{6}\nLocalità: {7}\nResort: {8}\nPrezzo per persona: {9}€\nPartecipanti: {10}\nResponsabile viaggio: {11}".format(self.nome_viaggio,self.giorno_p,self.mese_p,self.anno_p,self.giorno_r,self.mese_r,self.anno_r,self.localita,self.resort,self.prezzo,self.partecipanti,self.responsabile_viaggio))
 
     def periodo(self):
-        self.data_partenza = date(self.giorno_p, self.mese_p, self.anno_p)
-        self.data_ritorno = date(self.giorno_r, self.mese_r, self.anno_r)
-        delta = self.data_ritorno - self.data_partenza
-        print('Giorni di vacanza programmati: {0}'.format( (delta.days) ))
+        self.data_partenza = date(self.anno_p, self.mese_p, self.giorno_p)
+        self.data_ritorno = date(self.anno_r, self.mese_r, self.giorno_r)
+        differenza = self.data_ritorno - self.data_partenza
+        print('Giorni di vacanza programmati: {0}'.format( (differenza.days) ))
+
+    def guadagno(self):
+        spese = (self.prezzo/100)*47
+        guadagno = self.prezzo-spese
+        print('\nGuadagno: {0}€'.format(guadagno))
+
 
 
 class Vacanza_invernale(Viaggio):
-    def __init__(self, nome_viaggio, data_partenza, data_ritorno, localita, resort, prezzo, partecipanti, responsabile_viaggio, skipass, impianti_sciistici):
-        super().__init__(nome_viaggio, data_partenza, data_ritorno, localita, resort, prezzo, partecipanti, responsabile_viaggio)
+    def __init__(self, nome_viaggio, giorno_p, mese_p, anno_p, giorno_r, mese_r, anno_r, localita, resort, prezzo, partecipanti, responsabile_viaggio, skipass, impianti_sciistici):
+        super().__init__(nome_viaggio, giorno_p, mese_p, anno_p, giorno_r, mese_r, anno_r, localita, resort, prezzo, partecipanti, responsabile_viaggio)
         self.skipass = skipass
         self.impianti_sciistici = impianti_sciistici
+        self.prezzo2 = self.prezzo + ( (self.prezzo/100)*15 )
 
     def stampa(self):
-        print("Informazioni viaggio:\n\nTitolo: {0}\nPartenza: {1}\nRitorno: {2}\nLocalità: {3}\nResort: {4}\nPrezzo: {5}\nPartecipanti: {6}\nResponsabile viaggio: {7}\n\nPrezzo skipass giornaliero: {8}€\nImpianto sciistico: {9}".format(self.nome_viaggio,self.data_partenza,self.data_ritorno,self.localita,self.resort,self.prezzo,self.partecipanti,self.responsabile_viaggio, self.skipass, self.impianti_sciistici))
+        print("Informazioni viaggio:\n\nTitolo: {0}\nPartenza: {1}/{2}/{3}\nRitorno: {4}/{5}/{6}\nLocalità: {7}\nResort: {8}\nPrezzo per persona: {9}€\nPartecipanti: {10}\nResponsabile viaggio: {11}\n\nPrezzo skipass giornaliero: {12}€\nImpianto sciistico: {13}\n\nPrezzo con skipass: {14}€".format(self.nome_viaggio,self.giorno_p,self.mese_p,self.anno_p,self.giorno_r,self.mese_r,self.anno_r,self.localita,self.resort,self.prezzo,self.partecipanti,self.responsabile_viaggio, self.skipass, self.impianti_sciistici, self.prezzo2))
 
 
 
 class Vacanza_estiva(Viaggio):
-    def __init__(self, nome_viaggio, data_partenza, data_ritorno, localita, resort, prezzo, partecipanti, responsabile_viaggio, distanza, escursioni_marine):
-        super().__init__(nome_viaggio, data_partenza, data_ritorno, localita, resort, prezzo, partecipanti, responsabile_viaggio) 
+    def __init__(self, nome_viaggio, giorno_p, mese_p, anno_p, giorno_r, mese_r, anno_r, localita, resort, prezzo, partecipanti, responsabile_viaggio, distanza, escursioni_marine):
+        super().__init__(nome_viaggio, giorno_p, mese_p, anno_p, giorno_r, mese_r, anno_r, localita, resort, prezzo, partecipanti, responsabile_viaggio) 
         self.distanza = distanza
         self.escursioni_marine = escursioni_marine
+        self.prezzo2 = self.prezzo + ( (self.prezzo/100)*10 )
+
+    def stampa(self):
+        print("Informazioni viaggio:\n\nTitolo: {0}\nPartenza: {1}/{2}/{3}\nRitorno: {4}/{5}/{6}\nLocalità: {7}\nResort: {8}\nPrezzo per persona: {9}€\nPartecipanti: {10}\nResponsabile viaggio: {11}\n\nDistanza resort-spiaggia: {12}m\nEscursioni marine: {13},{14},{15}\n\nPrezzo con escursioni_marine: {16}€".format(self.nome_viaggio,self.giorno_p,self.mese_p,self.anno_p,self.giorno_r,self.mese_r,self.anno_r,self.localita,self.resort,self.prezzo,self.partecipanti,self.responsabile_viaggio,self.distanza,self.escursioni_marine[0], self.escursioni_marine[1],self.escursioni_marine[2],self.prezzo2))
+
+
+
+
 
 print("\n===============1°=VIAGGIO=================")
-viaggio1 = Viaggio('Avventura nella giungla', [23,6,2034], [12,7,2034], 'Burundi', 'Uga Buga', '245€', 'Pino, Ugo, Gianfranca', 'Giuliano Caravagno')
+viaggio1 = Viaggio('Avventura nella giungla', 23,6,2034, 12,7,2034, 'Burundi', 'Uga Buga', 245, 'Pino, Ugo, Gianfranca', 'Giuliano Caravagno')
 viaggio1.stampa()
 viaggio1.periodo()
+viaggio1.guadagno()
 
 print("\n===============2°=VIAGGIO=================")
-viaggio_invernale = Vacanza_invernale('Rotoliamo nella neve!', (15,11,2024), (14,12,2024), 'Triglav', 'Resort Triglav', '670€', 'Bruno, Jože, Svetina', 'Stefano Alberto Sloveno', 56, 'Triglavski smučarski park')
+viaggio_invernale = Vacanza_invernale('Rotoliamo nella neve!', 15,11,2024, 14,12,2024, 'Triglav', 'Resort Triglav', 670, 'Bruno, Jože, Svetina', 'Stefano Alberto Sloveno', 56, 'Triglavski smučarski park')
 viaggio_invernale.stampa()
 viaggio_invernale.periodo()
+viaggio_invernale.guadagno()
+
+print("\n===============3°=VIAGGIO=================")
+viaggio_estivo = Vacanza_estiva('Quanto si suda in Sudan', 23,7,2023, 19,8,2023, 'Sudan', 'Resort Aziz', 130, 'Paolo, Annamaria', 'Aziz Zizà', 400, ['snorkeling', 'surfing', 'danza tribù'])
+viaggio_estivo.stampa()
+viaggio_estivo.periodo()
+viaggio_estivo.guadagno()
+
+
+
+def funzione_di_input():
+
+    titolo1 = 'Viaggio invernale', titolo2 = 'Viaggio estivo'
+    giorno_p=0,mese_p=0,anno_p=0,giorno_r=0,mese_r=0,anno_r=0
+    localita='',resort='',prezzo=0,partecipanti='',responsabile_viaggio=''
+    prezzo_skipass=0,impianti_sciistici=''
+    distanza=0,escursioni_marine=[]
+
+    print("Scegliere il tipo di vacanza:\n[1] per vacanza invernale\n[2] per vacanza estiva")
+    duo = input()
+
+    if(duo==1):
+        
+        print('Inserire giorno(g), mese(m), anno(aaaa) di partenza:')
+        giorno_p = input()
+        mese_p = input()
+        anno_p = input()
+        print('Inserire giorno(g), mese(m), anno(aaaa) di ritorno:')
+        giorno_r = input()
+        mese_r = input()
+        anno_r = input()
+
+
+    elif(duo==2):
+        
+        print('Inserire giorno(g), mese(m), anno(aaaa) di partenza:')
+        giorno_p = input()
+        mese_p = input()
+        anno_p = input()
+        print('Inserire giorno(g), mese(m), anno(aaaa) di ritorno:')
+        giorno_r = input()
+        mese_r = input()
+        anno_r = input()
+
+    else:
+        print("Inserire un numero valido!")
+        funzione_di_input()
+
+funzione_di_input()
